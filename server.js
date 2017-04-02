cbdc6920910ae76b28c3dbe9f9733e08d6779417
Nigel Thornton Clark <nigeltc@gmail.com>
2017-04-02 10:09:21 -0400

    added zipcode

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
 var http = require('http');
+var zipcode = require('zipcode');
 
 http.createServer( function( req, res ) {
     fs.readFile( 'index.html', function( err, data ) {
 	res.writeHead( 200, {'Content-Type': 'text/html'} );
+	res.write( 'zip = ' + zipcode.lookup( '08540' ) );
 	res.end( data );
     });
 }).listen( 1337, '127.0.0.1' );
